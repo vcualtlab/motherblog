64f12593e3e4ad26149207ea85178d83450fa53f
Tom Woodward <twwoodward@me.com>
2017-02-01 22:49:07 -0500

    warning if FWP isn't active
    some modal popup, scroll to fwp stuff that I can probably make cleaner
    but seems functional now

--- admin/js/altlab-motherblog-admin.js
+++ admin/js/altlab-motherblog-admin.js
@@ -28,5 +28,7 @@
 	 * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
 	 * be doing this, we should try to minimize doing that in our own work.
 	 */
+		
 
 })( jQuery );
+
--- altlab-motherblog.php
+++ altlab-motherblog.php
@@ -30,6 +30,73 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+function motherblog_admin_script() {
+           wp_enqueue_script( 'altlab-motherblog', plugin_dir_url( __FILE__ ) . 'admin/js/altlab-motherblog-admin.js', array(), '1.0' );
+}
+add_action( 'admin_enqueue_scripts', 'motherblog_admin_script' );
+
+//warn if FWP isn't active from this stackoverflow question http://wordpress.stackexchange.com/questions/127818/how-to-make-a-plugin-require-another-plugin
+add_action( 'admin_init', 'child_plugin_has_parent_plugin' );
+function child_plugin_has_parent_plugin() {
+    if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'feedwordpress/feedwordpress.php' ) ) {
+        add_action( 'admin_notices', 'child_plugin_notice' );
+
+        deactivate_plugins( plugin_basename( __FILE__ ) ); 
+
+        if ( isset( $_GET['activate'] ) ) {
+            unset( $_GET['activate'] );
+        }
+    }
+}
+
+function child_plugin_notice(){
+    ?><div class="modal" id="modal"> <button class="close-button" id="close-button" onclick="getEm()">Close this warning</button>
+  		<div class="modal-guts">
+  			<h2>Your Attention Please</h2>
+  			<p>The ALT Lab MotherBlog plugin requires the FeedWordPress plugin to be installed and active. 
+        I humbly ask you to activate FeedWordPress and then re-activate this plugin.</p>
+        <p>FeedWordPress will be highlighted in <span class="fwp-focus">green</span> and after you close this window you'll probably automatically scroll down to it.</p>
+  		</div>
+  		<!--UGLY I know but it was late and the normal paths were not working-->
+  		<script  type="text/javascript" charset="utf-8" >
+  		
+      var modal = document.querySelector("#modal");
+      var modalOverlay = document.querySelector("#modal-overlay");
+      var closeButton = document.querySelector("#close-button");
+      var openButton = document.querySelector("#open-button");
+
+        closeButton.addEventListener("click", function() {
+        modal.classList.toggle("closed");
+        modalOverlay.classList.toggle("closed");
+        getEm();
+        console.log('foo');
+      });
+
+        function getEm() {
+            var b = document.getElementsByTagName("strong");         
+            for (var i in b)
+                if (b.hasOwnProperty(i)) {
+                    var plugin = (b[i].innerHTML);
+                    console.log(plugin);           
+                    if (plugin === "FeedWordPress") {
+                        b[i].setAttribute("id", "fwp-focus");
+
+                    }
+                }                
+        }
+
+        jQuery("#close-button").click(function() {
+        jQuery('html, body').animate({
+            scrollTop: jQuery("#fwp-focus").offset().top-200}, 2000);
+        });
+
+  		</script>
+  	</div>
+  	<?php
+}
+
+
+
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-altlab-motherblog-activator.php
